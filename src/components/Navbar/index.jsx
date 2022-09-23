ef1a58f465181696ec644dd2bb2e27dbed5daa22
tajhmartin1 <tajhmartin87@gmail.com>
2022-09-23 18:23:42 -0400

    copy code

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Nav, NavLink, NavMenu } 
+    from "./NavbarElements";
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/tutorial" activeStyle>
+            Sign Up
+          </NavLink>
+          <NavLink to="/blogs" activeStyle>
+            Blogs
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
